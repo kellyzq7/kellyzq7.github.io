896337917854712504c11b936e3f85b75d9440cf
kellyzq7 <kellyzq7@gmail.com>
2025-09-30 00:18:05 -0700

    Added images

--- src/pages/Activities.js
+++ src/pages/Activities.js
@@ -6,7 +6,21 @@ import TypingText from "../components/TypingText";
 import VH1 from "../images/vh1.jpg";
 import VH2 from "../images/vh2.jpg";
 import VH3 from "../images/vh3.jpg";
-
+import OH1 from "../images/OH1.jpg";
+import OH2 from "../images/OH2.jpg";
+import OH3 from "../images/OH3.JPG";
+import ASG1 from "../images/asg1.png";
+import ASG2 from "../images/asg2.png";
+import ASG3 from "../images/asg3.png";
+import MC1 from "../images/mc1.JPG";
+import MC2 from "../images/mc2.JPG";
+import MC3 from "../images/mc3.jpg";
+import BC1 from "../images/bc1.JPG";
+import BC2 from "../images/bc2.JPG";
+import BC3 from "../images/bc3.JPG";
+import ATDP1 from "../images/atdp1.jpg";
+import ATDP2 from "../images/atdp2.JPG";
+import ATDP3 from "../images/atdp3.jpg";
 
 const ActivitiesPage = () => {
   const activities = [
@@ -18,27 +32,27 @@ const ActivitiesPage = () => {
     {
       title: "Ohlone Hacks 2024",
       description: "This is a description for Activity 2.",
-      images: ["/images/act2-1.jpg", "/images/act2-2.jpg", "/images/act2-3.jpg"],
+      images: [OH1, OH2, OH3],
     },
     {
       title: "Associated Student Government",
       description: "This is a description for Activity 2.",
-      images: ["/images/act2-1.jpg", "/images/act2-2.jpg", "/images/act2-3.jpg"],
+      images: [ASG1, ASG2, ASG3],
     },
     {
       title: "Math Club",
       description: "This is a description for Activity 2.",
-      images: ["/images/act2-1.jpg", "/images/act2-2.jpg", "/images/act2-3.jpg"],
+      images: [MC1, MC2, MC3],
     },
     {
       title: "Book Club",
       description: "This is a description for Activity 2.",
-      images: ["/images/act2-1.jpg", "/images/act2-2.jpg", "/images/act2-3.jpg"],
+      images: [BC1, BC2, BC3],
     },
     {
       title: "Berkeley ATDP",
       description: "This is a description for Activity 2.",
-      images: ["/images/act2-1.jpg", "/images/act2-2.jpg", "/images/act2-3.jpg"],
+      images: [ATDP1, ATDP2, ATDP3],
     },
   ];
 
